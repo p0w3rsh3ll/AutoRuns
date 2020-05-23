ac3c7b16cb4d220083ccb9bef53ef2323aae053d
Emin <emin.atac@gmail.com>
2020-05-23 14:17:03 +0200

    Update Get-FileHash function from 5.1 (see issue #77)

--- AutoRuns.psm1
+++ AutoRuns.psm1
@@ -221,71 +221,149 @@ Begin {
 
     if ($PSVersionTable.PSEdition -ne 'Core') {
     # Courtesy of Microsoft
-    # Extracted from PS 4.0 with (dir function:\Get-FileHash).Definition
-    Function Get-FileHash {
-        [CmdletBinding(DefaultParameterSetName = 'Path')]
-        Param(
-            [Parameter(Mandatory, ParameterSetName='Path', Position = 0)]
-            [System.String[]]
-            $Path,
+    # Extracted from PS 5.1 with (dir function:\Get-FileHash).Definition
+        function Get-FileHash
+        {
+            [CmdletBinding(DefaultParameterSetName = "Path", HelpURI = "https://go.microsoft.com/fwlink/?LinkId=517145")]
+            param(
+                [Parameter(Mandatory, ParameterSetName="Path", Position = 0)]
+                [System.String[]]
+                $Path,
 
-            [Parameter(Mandatory, ParameterSetName='LiteralPath', ValueFromPipelineByPropertyName = $true)]
-            [Alias('PSPath')]
-            [System.String[]]
-            $LiteralPath,
+                [Parameter(Mandatory, ParameterSetName="LiteralPath", ValueFromPipelineByPropertyName = $true)]
+                [Alias("PSPath")]
+                [System.String[]]
+                $LiteralPath,
+        
+                [Parameter(Mandatory, ParameterSetName="Stream")]
+                [System.IO.Stream]
+                $InputStream,
 
-            [ValidateSet('SHA1', 'SHA256', 'SHA384', 'SHA512', 'MACTripleDES', 'MD5', 'RIPEMD160')]
-            [System.String]
-            $Algorithm='SHA256'
-        )
-
-        Begin {
-            # Construct the strongly-typed crypto object
-            $hasher = [System.Security.Cryptography.HashAlgorithm]::Create($Algorithm)
-        }
-        Process {
-            $pathsToProcess = @()
-
-            if($PSCmdlet.ParameterSetName  -eq 'LiteralPath') {
-                $pathsToProcess += Resolve-Path -LiteralPath $LiteralPath | Foreach-Object ProviderPath
-            } else {
-                $pathsToProcess += Resolve-Path $Path | Foreach-Object ProviderPath
-            }
-
-            foreach($filePath in $pathsToProcess) {
-                if(Test-Path -LiteralPath $filePath -PathType Container) {
-                    continue
+                [ValidateSet("SHA1", "SHA256", "SHA384", "SHA512", "MACTripleDES", "MD5", "RIPEMD160")]
+                [System.String]
+                $Algorithm="SHA256"
+            )
+    
+            begin
+            {
+                # Construct the strongly-typed crypto object
+        
+                # First see if it has a FIPS algorithm  
+                $hasherType = "System.Security.Cryptography.${Algorithm}CryptoServiceProvider" -as [Type]
+                if ($hasherType)
+                {
+                    $hasher = $hasherType::New()
                 }
-
-                try {
-                    # Read the file specified in $FilePath as a Byte array
-                    [system.io.stream]$stream = [system.io.file]::OpenRead($FilePath)
-
-                    # Compute file-hash using the crypto object
-                    [Byte[]] $computedHash = $hasher.ComputeHash($stream)
-                } catch [Exception] {
-                    $errorMessage = [Microsoft.PowerShell.Commands.UtilityResources]::FileReadError -f $FilePath, $_
-                    Write-Error -Message $errorMessage -Category ReadError -ErrorId 'FileReadError' -TargetObject $FilePath
-                    return
-                } finally {
-                    if($stream) {
-                        $stream.Close()
+                else
+                {
+                    # Check if the type is supported in the current system
+                    $algorithmType = "System.Security.Cryptography.${Algorithm}" -as [Type]
+                    if ($algorithmType)
+                    {
+                        if ($Algorithm -eq "MACTripleDES")
+                        {
+                            $hasher = $algorithmType::New()
+                        }
+                        else
+                        {
+                            $hasher = $algorithmType::Create()
+                        }
+                    }
+                    else
+                    {
+                        $errorId = "AlgorithmTypeNotSupported"
+                        $errorCategory = [System.Management.Automation.ErrorCategory]::InvalidArgument
+                        $errorMessage = [Microsoft.PowerShell.Commands.UtilityResources]::AlgorithmTypeNotSupported -f $Algorithm
+                        $exception = [System.InvalidOperationException]::New($errorMessage)
+                        $errorRecord = [System.Management.Automation.ErrorRecord]::New($exception, $errorId, $errorCategory, $null)
+                        $PSCmdlet.ThrowTerminatingError($errorRecord)
                     }
                 }
 
-                # Convert to hex-encoded string
-                [string] $hash = [BitConverter]::ToString($computedHash) -replace '-',''
+                function GetStreamHash
+                {
+                    param(
+                        [System.IO.Stream]
+                        $InputStream,
 
-                $retVal = [PSCustomObject] @{
-                    Algorithm = $Algorithm.ToUpperInvariant()
-                    Hash = $hash
-                    Path = $filePath
+                        [System.String]
+                        $RelatedPath,
+
+                        [System.Security.Cryptography.HashAlgorithm]
+                        $Hasher)
+
+                    # Compute file-hash using the crypto object
+                    [Byte[]] $computedHash = $Hasher.ComputeHash($InputStream)
+                    [string] $hash = [BitConverter]::ToString($computedHash) -replace '-',''
+
+                    if ($RelatedPath -eq $null)
+                    {
+                        $retVal = [PSCustomObject] @{
+                            Algorithm = $Algorithm.ToUpperInvariant()
+                            Hash = $hash
+                        }
+                    }
+                    else
+                    {
+                        $retVal = [PSCustomObject] @{
+                            Algorithm = $Algorithm.ToUpperInvariant()
+                            Hash = $hash
+                            Path = $RelatedPath
+                        }
+                    }
+                    $retVal.psobject.TypeNames.Insert(0, "Microsoft.Powershell.Utility.FileHash")
+                    $retVal
                 }
-                $retVal.psobject.TypeNames.Insert(0, 'Microsoft.Powershell.Utility.FileHash')
-                $retVal
+            }
+    
+            process
+            {
+                if($PSCmdlet.ParameterSetName -eq "Stream")
+                {
+                    GetStreamHash -InputStream $InputStream -RelatedPath $null -Hasher $hasher
+                }
+                else
+                {
+                    $pathsToProcess = @()
+                    if($PSCmdlet.ParameterSetName  -eq "LiteralPath")
+                    {
+                        $pathsToProcess += Resolve-Path -LiteralPath $LiteralPath | Foreach-Object ProviderPath
+                    }
+                    if($PSCmdlet.ParameterSetName -eq "Path")
+                    {
+                        $pathsToProcess += Resolve-Path $Path | Foreach-Object ProviderPath
+                    }
+
+                    foreach($filePath in $pathsToProcess)
+                    {
+                        if(Test-Path -LiteralPath $filePath -PathType Container)
+                        {
+                            continue
+                        }
+
+                        try
+                        {
+                            # Read the file specified in $FilePath as a Byte array
+                            [system.io.stream]$stream = [system.io.file]::OpenRead($filePath)
+                            GetStreamHash -InputStream $stream  -RelatedPath $filePath -Hasher $hasher
+                        }
+                        catch [Exception]
+                        {
+                            $errorMessage = [Microsoft.PowerShell.Commands.UtilityResources]::FileReadError -f $FilePath, $_
+                            Write-Error -Message $errorMessage -Category ReadError -ErrorId "FileReadError" -TargetObject $FilePath
+                            return
+                        }
+                        finally
+                        {
+                            if($stream)
+                            {
+                                $stream.Dispose()
+                            }
+                        }                            
+                    }
+                }
             }
         }
-    }
     }
     Function Get-RegValue {
     [CmdletBinding()]
