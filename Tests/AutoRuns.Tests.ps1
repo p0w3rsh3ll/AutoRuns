c4c6c242cacdb78696d9a01130664be0309b35b8
p0w3rsh3ll <emin.atac@gmail.com>
2020-10-11 22:26:04 +0200

    Pester test for issue #80

--- Tests/AutoRuns.Tests.ps1
+++ Tests/AutoRuns.Tests.ps1
@@ -64,6 +64,23 @@ Describe 'Testing Get-PSPrettyAutorun for Print Monitors' {
 
 Describe 'Testing ScheduledTasks' {
 
+    Context 'Inside Add-PSAutoRunHash' {
+        It 'issue 80 should be solved' {
+            Mock -CommandName Get-PSRawAutoRun -MockWith {
+                return [PSCustomObject]@{
+                    Path = 'C:\WINDOWS\system32\Tasks\\ConfigAppIdSvc'
+                    Item = 'ConfigAppIdSvc'
+                    Category = 'Task'
+                    Value = 'whatever'
+                    # ImagePath = '-Command "& ''C:\Users\username\Documents\script.ps1'
+                    ImagePath = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -Exec Bypass -Command "Set-Service -Name AppIDSvc -StartupType Automatic"'
+                }
+            } -ParameterFilter { $ScheduledTasks -eq [switch]::Present }
+
+            { Get-PSRawAutoRun -ScheduledTasks | Add-PSAutoRunHash -ShowFileHash -ErrorAction Stop } | should not throw
+        }
+    }
+
 # Emulate what Get-PSRawAutoRun -ScheduledTasks returns & pass it to Get-PSPrettyAutorun
 
     Context 'Inside Get-PSRawAutoRun' {
